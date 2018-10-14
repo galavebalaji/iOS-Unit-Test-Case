588cf0646ad7f751d1f6421de79baf408a92d9f1
galavebalaji <balaji.sonora@gmail.com>
2018-10-14 12:26:05 +0530

    Add files via upload

---
+++ DemoTestCaseTests/DemoTestCaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  DemoTestCaseTests.swift
+//  DemoTestCaseTests
+//
+//  Created by Balaji Galave on 10/14/18.
+//  Copyright © 2018 Globant. All rights reserved.
+//
+
+import XCTest
+@testable import DemoTestCase
+
+class DemoTestCaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
